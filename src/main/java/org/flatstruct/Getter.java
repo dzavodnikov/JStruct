dzavodnikov/StructCollection

Add structure definition through interface

src/main/java/org/jstruct/Getter.java
@@ -13,26 +13,22 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.jstruct.example;
+package org.jstruct;
 
-import org.jstruct.Getter;
-import org.jstruct.Setter;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
- * Testing structure.
+ * Define getter for the field.
  */
-public interface Point2D {
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Getter {
 
-    String X_FIELD_NAME = "x";
-    String Y_FIELD_NAME = "y";
-
-    void setX(@Setter(X_FIELD_NAME) int x);
-
-    @Getter(X_FIELD_NAME)
-    int getX();
-
-    void setY(@Setter(Y_FIELD_NAME) int y);
-
-    @Getter(Y_FIELD_NAME)
-    int getY();
+    /**
+     * @return name of the field.
+     */
+    String value();
 }

src/main/java/org/jstruct/Setter.java
@@ -13,26 +13,22 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.jstruct.example;
+package org.jstruct;
 
-import org.jstruct.Getter;
-import org.jstruct.Setter;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
- * Testing structure.
+ * Define getter for the field.
  */
-public interface Point2D {
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.PARAMETER)
+public @interface Setter {
 
-    String X_FIELD_NAME = "x";
-    String Y_FIELD_NAME = "y";
-
-    void setX(@Setter(X_FIELD_NAME) int x);
-
-    @Getter(X_FIELD_NAME)
-    int getX();
-
-    void setY(@Setter(Y_FIELD_NAME) int y);
-
-    @Getter(Y_FIELD_NAME)
-    int getY();
+    /**
+     * @return name of the field.
+     */
+    String value();
 }

dzavodnikov/JStruct

src/main/java/org/flatstruct/Getter.java
@@ -13,26 +13,22 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.flatstruct.example.structure;
+package org.flatstruct;
 
-import org.flatstruct.Getter;
-import org.flatstruct.Setter;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
- * Testing structure.
+ * Define getter for the field.
  */
-public interface Point2D {
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Getter {
 
-    String X_FIELD_NAME = "x";
-    String Y_FIELD_NAME = "y";
-
-    void setX(@Setter(X_FIELD_NAME) int x);
-
-    @Getter(X_FIELD_NAME)
-    int getX();
-
-    void setY(@Setter(Y_FIELD_NAME) int y);
-
-    @Getter(Y_FIELD_NAME)
-    int getY();
+    /**
+     * @return name of the field.
+     */
+    String value();
 }

src/main/java/org/flatstruct/Setter.java
@@ -13,26 +13,22 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.flatstruct.example.structure;
+package org.flatstruct;
 
-import org.flatstruct.Getter;
-import org.flatstruct.Setter;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
- * Testing structure.
+ * Define getter for the field.
  */
-public interface Point2D {
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.PARAMETER)
+public @interface Setter {
 
-    String X_FIELD_NAME = "x";
-    String Y_FIELD_NAME = "y";
-
-    void setX(@Setter(X_FIELD_NAME) int x);
-
-    @Getter(X_FIELD_NAME)
-    int getX();
-
-    void setY(@Setter(Y_FIELD_NAME) int y);
-
-    @Getter(Y_FIELD_NAME)
-    int getY();
+    /**
+     * @return name of the field.
+     */
+    String value();
 }